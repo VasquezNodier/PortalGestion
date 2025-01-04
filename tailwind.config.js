c22d6f80f00112fa1a79df978aa7f901f1360000
Nodier Vasquez <vasqueznap@gmail.com>
2025-01-04 09:09:36 -0500

    [CONF] extend - colors tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#00524a',
+        'secondary': '#11836d',
+        'tertiary': '#e6ffe6',
+
+        'info': '#17a2b8',
+        'danger': '#dc3545',
+        'success': '#28a745',
+        'warning': '#ffc107',
+
+      }
+    },
   },
   plugins: [],
 }
